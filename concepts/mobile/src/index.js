54880e41598bb6474e2c88dc8cf88e484b2b6440
Murilo Cunha <murilo.sant@hotmail.com>
2020-08-19 19:42:56 -0300

    feat(react-native-concepts): integrate API

--- concepts/mobile/src/index.js
+++ concepts/mobile/src/index.js
@@ -1,20 +1,58 @@
-import React from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  SafeAreaView,
+  FlatList,
+  Text,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
 
-// Não possuem valor semântico (significado)
-// Não possuem estlização própria
-// Todos componentes possuem por padrão "display: flex"
-
-// View: div, footer, header, main, aside, section
-// Text: p, span, strong, h1, h2, h3
+import api from './services/api';
 
 export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then((response) => {
+      setProjects(response.data);
+      console.log(response.data);
+    });
+  }, []);
+
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: 'Murilo Cunha',
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello GoStack</Text>
-      </View>
+
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={(project) => project.id}
+          renderItem={({ item: project }) => (
+            <Text key={project.id} style={styles.project}>
+              {project.title}
+            </Text>
+          )}
+        />
+
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={styles.button}
+          onPress={handleAddProject}>
+          <Text style={styles.buttonText}>Adicionar projeto</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
     </>
   );
 }
@@ -23,13 +61,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#7159c1',
+  },
+
+  project: {
+    color: '#FFF',
+    fontSize: 30,
+  },
+
+  button: {
+    backgroundColor: '#fff',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
     justifyContent: 'center',
     alignItems: 'center',
   },
 
-  title: {
-    color: '#FFF',
-    fontSize: 32,
+  buttonText: {
     fontWeight: 'bold',
+    fontSize: 16,
   },
 });
