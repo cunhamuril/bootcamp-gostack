89a148409711a157ad8da5160f704f7da4ecc7c5
Murilo Cunha <murilo.sant@hotmail.com>
2020-08-24 17:59:08 -0300

    refactor(first-project-node): isolate services and added one of SOLID principle

--- first-project-node/src/routes/appointmentes.routes.ts
+++ first-project-node/src/routes/appointmentes.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
-import { startOfHour, parseISO } from 'date-fns';
+import { parseISO } from 'date-fns';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
-
 const appointmentsRepository = new AppointmentsRepository();
+
+// Rota: Receber a requisição, chamar outro arquivo, devolver uma resposta.
 
 appointmentsRouter.get('/', (request, response) => {
   const appointments = appointmentsRepository.all();
@@ -14,26 +16,24 @@ appointmentsRouter.get('/', (request, response) => {
 });
 
 appointmentsRouter.post('/', (request, response) => {
-  const { provider, date } = request.body;
+  try {
+    const { provider, date } = request.body;
 
-  const parserdDate = startOfHour(parseISO(date));
+    const parserdDate = parseISO(date);
 
-  const findAppointmentInSameDate = appointmentsRepository.findByDate(
-    parserdDate,
-  );
+    const createAppointment = new CreateAppointmentService(
+      appointmentsRepository,
+    );
 
-  if (findAppointmentInSameDate) {
-    return response
-      .status(400)
-      .json({ message: 'This appointment is already booked' });
+    const appointment = createAppointment.execute({
+      provider,
+      date: parserdDate,
+    });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
   }
-
-  const appointment = appointmentsRepository.create({
-    provider,
-    date: parserdDate,
-  });
-
-  return response.json(appointment);
 });
 
 export default appointmentsRouter;
