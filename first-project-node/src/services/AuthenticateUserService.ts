6d00bcebbff4a34c04123fcaa740bb3cbc7d8eb3
Murilo Cunha <murilo.sant@hotmail.com>
2020-08-27 00:25:08 -0300

    feat(authentication): create sessions route and authetication with JWT

---
+++ first-project-node/src/services/AuthenticateUserService.ts
@@ -0,0 +1,47 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    // 19ac7d307d0e3ce9e6a526dc6e511ea1 => gerado com md5 online
+
+    const token = sign({}, '19ac7d307d0e3ce9e6a526dc6e511ea1', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default AuthenticateUserService;
