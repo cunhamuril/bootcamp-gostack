0f335210a67e62714ce0fdd484af115c17f8007a
Murilo Cunha <murilo.sant@hotmail.com>
2020-09-01 15:02:17 -0300

    feat(dashboard): diagrammed page

--- first-react-project/src/pages/Dashboard/index.tsx
+++ first-react-project/src/pages/Dashboard/index.tsx
@@ -1,9 +1,66 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no GitHub</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no GitHub</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="#!">
+          <img
+            src="https://avatars3.githubusercontent.com/u/49242233?s=460&v=4"
+            alt="Murilo Cunha"
+          />
+
+          <div>
+            <strong>cunhamuril/next-level-week-2</strong>
+            <p>Next Level Week 2 by Rocketseat - App Proffy</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="#!">
+          <img
+            src="https://avatars3.githubusercontent.com/u/49242233?s=460&v=4"
+            alt="Murilo Cunha"
+          />
+
+          <div>
+            <strong>cunhamuril/next-level-week-2</strong>
+            <p>Next Level Week 2 by Rocketseat - App Proffy</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="#!">
+          <img
+            src="https://avatars3.githubusercontent.com/u/49242233?s=460&v=4"
+            alt="Murilo Cunha"
+          />
+
+          <div>
+            <strong>cunhamuril/next-level-week-2</strong>
+            <p>Next Level Week 2 by Rocketseat - App Proffy</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
--- first-react-project/src/pages/Dashboard/styles.ts
+++ first-react-project/src/pages/Dashboard/styles.ts
@@ -1,6 +1,103 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Title = styled.h1`
+  margin-top: 80px;
+
   font-size: 48px;
+  max-width: 450px;
+  line-height: 56px;
+
   color: #3a3a3a;
+`;
+
+export const Form = styled.form`
+  display: flex;
+
+  margin-top: 40px;
+  max-width: 700px;
+
+  input {
+    flex: 1;
+    border: 0;
+    height: 70px;
+    padding: 0 24px;
+    border-radius: 5px 0 0 5px;
+
+    color: #3a3a3a;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    border: 0;
+    width: 210px;
+    height: 70px;
+
+    color: #fff;
+    font-weight: bold;
+    background: #04d361;
+    border-radius: 0 5px 5px 0;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background: ${shade(0.2, '#04d361')};
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+
+  max-width: 700px;
+
+  a {
+    display: flex;
+    align-items: center;
+
+    width: 100%;
+    padding: 24px;
+    border-radius: 5px;
+
+    background: #fff;
+    text-decoration: none;
+    transition: transform 0.2s;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        margin-top: 4px;
+
+        font-size: 18px;
+        color: #a8a8b3;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
 `;
