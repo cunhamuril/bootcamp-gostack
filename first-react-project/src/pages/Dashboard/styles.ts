fdb8dad797c9486b5439a668a407f8c16dc30e83
Murilo Cunha <murilo.sant@hotmail.com>
2020-09-01 16:15:26 -0300

    feat(dashboard): integrate with GitHub API and persist data in localStorage

--- first-react-project/src/pages/Dashboard/styles.ts
+++ first-react-project/src/pages/Dashboard/styles.ts
@@ -1,5 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
   margin-top: 80px;
@@ -11,7 +15,7 @@ export const Title = styled.h1`
   color: #3a3a3a;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   display: flex;
 
   margin-top: 40px;
@@ -19,10 +23,17 @@ export const Form = styled.form`
 
   input {
     flex: 1;
-    border: 0;
     height: 70px;
     padding: 0 24px;
     border-radius: 5px 0 0 5px;
+    border: 2px solid #fff;
+    border-right: 0;
+
+    ${props =>
+      props.hasError &&
+      css`
+        border-color: #c53030;
+      `}
 
     color: #3a3a3a;
 
@@ -46,6 +57,12 @@ export const Form = styled.form`
       background: ${shade(0.2, '#04d361')};
     }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
 `;
 
 export const Repositories = styled.div`
@@ -80,7 +97,8 @@ export const Repositories = styled.div`
     }
 
     div {
-      margin-left: 16px;
+      flex: 1;
+      margin: 0 16px;
 
       strong {
         font-size: 20px;
