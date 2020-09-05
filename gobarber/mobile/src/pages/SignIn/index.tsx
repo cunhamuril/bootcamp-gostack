7f19c0d7bfcba2d733a74aee9e46fae965d6a070
Murilo Cunha <murilo.sant@hotmail.com>
2020-09-05 13:59:31 -0300

    feat(authentication): added screen styles and diagramation

--- gobarber/mobile/src/pages/SignIn/index.tsx
+++ gobarber/mobile/src/pages/SignIn/index.tsx
@@ -1,24 +1,62 @@
 import React from 'react';
-import { Image } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
 
 import logoImg from '../../assets/logo.png';
 
 import { Button, Input } from '../../components';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
 
-      <Title>Faça seu logon</Title>
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
 
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Senha" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
 
-      <Button onPress={() => {}}>Entrar</Button>
-    </Container>
+            <Button onPress={() => {}}>Entrar</Button>
+
+            <ForgotPassword>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton onPress={() => {}}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
--- gobarber/mobile/src/pages/SignIn/styles.ts
+++ gobarber/mobile/src/pages/SignIn/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
@@ -14,4 +15,39 @@ export const Title = styled.Text`
   color: #f4ede8;
   font-size: 24px;
   font-family: 'RobotoSlab-Medium';
+`;
+
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+
+export const ForgotPasswordText = styled.Text`
+  color: #f4ede8;
+  font-size: 16px;
+  font-family: 'RobotoSlab-Regular';
+`;
+
+export const CreateAccountButton = styled.TouchableOpacity`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  left: 0;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  padding: 16px 0 ${16 + getBottomSpace()}px;
+
+  background: #312e38;
+  border-top-width: 1px;
+  border-color: #232129;
+`;
+
+export const CreateAccountButtonText = styled.Text`
+  margin-left: 16px;
+
+  color: #ff9000;
+  font-size: 18px;
+  font-family: 'RobotoSlab-Regular';
 `;
