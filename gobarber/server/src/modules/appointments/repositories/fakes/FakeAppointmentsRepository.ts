0261789325f82a30144f855f069c594c997c838e
Murilo Cunha <murilo.sant@hotmail.com>
2020-09-17 01:02:30 -0300

    test(appointments): dates not equal

--- gobarber/server/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ gobarber/server/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,4 +1,5 @@
 import { uuid } from 'uuidv4';
+import { isEqual } from 'date-fns';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
@@ -13,9 +14,9 @@ class AppointmentsRepository implements IAppointmentsRepository {
   }
 
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = await this.appointments.find(appointment => {
-      return appointment.date === date;
-    });
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(appointment.date, date),
+    );
 
     return findAppointment;
   }
