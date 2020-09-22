69c63b0526a657218d9ca15e3d01852ccde16869
Murilo Cunha <murilo.sant@hotmail.com>
2020-09-22 15:40:03 -0300

    test(update-user-avatar): added tests

--- gobarber/server/src/modules/users/services/UpdateUserAvatarService.ts
+++ gobarber/server/src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,10 +1,9 @@
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
-
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import User from '../infra/typeorm/entities/User';
 import IUsersRepository from '../repositories/IUsersRepository';
-import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 
 interface IRequest {
   user_id: string;
