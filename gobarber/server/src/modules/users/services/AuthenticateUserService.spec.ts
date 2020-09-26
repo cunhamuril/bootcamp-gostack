98104c211bfc9e8724f2236f87da4cb9b76741fb
Murilo Cunha <murilo.sant@hotmail.com>
2020-09-26 15:46:03 -0300

    feat(send-forgot-password-service): password recuperation

--- gobarber/server/src/modules/users/services/AuthenticateUserService.spec.ts
+++ gobarber/server/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -45,7 +45,7 @@ describe('CreateUser', () => {
       fakeHashProvider,
     );
 
-    expect(
+    await expect(
       authenticateUser.execute({
         email: 'johndoe@example.com',
         password: '123456',
@@ -73,7 +73,7 @@ describe('CreateUser', () => {
       password: '123456',
     });
 
-    expect(
+    await expect(
       authenticateUser.execute({
         email: 'johndoe@example.com',
         password: '654321',
