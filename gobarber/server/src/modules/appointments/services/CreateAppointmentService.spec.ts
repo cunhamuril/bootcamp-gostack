ffa81f688b6a96913d9e7288c27c6a02e95123c6
Murilo Cunha <murilo.sant@hotmail.com>
2020-11-18 18:10:41 -0300

    refactor(notifications): refactoring tests

--- gobarber/server/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ gobarber/server/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,16 +1,21 @@
 import AppError from '@shared/errors/AppError';
 
+import FakeNotificationsRepository from '@modules/notifications/repositories/fakes/FakeNotificationsRepository';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
 let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let fakeNotificationsRepository: FakeNotificationsRepository;
 let createAppointment: CreateAppointmentService;
 
 describe('CreateAppointment', () => {
   beforeEach(() => {
     fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    fakeNotificationsRepository = new FakeNotificationsRepository();
+
     createAppointment = new CreateAppointmentService(
       fakeAppointmentsRepository,
+      fakeNotificationsRepository,
     );
   });
 
@@ -30,7 +35,7 @@ describe('CreateAppointment', () => {
   });
 
   it('should not be able to create two appointments on the same time', async () => {
-    const appointmentDate = new Date(2020, 10, 17, 12);
+    const appointmentDate = new Date(2020, 12, 17, 12);
 
     await createAppointment.execute({
       date: appointmentDate,
