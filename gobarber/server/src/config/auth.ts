c66f779e678f1c589a7e06e1436c146903880544
Murilo Cunha <murilo.sant@hotmail.com>
2020-11-19 12:06:25 -0300

    feat(envs): added dotenv

--- gobarber/server/src/config/auth.ts
+++ gobarber/server/src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: '19ac7d307d0e3ce9e6a526dc6e511ea1', // => gerado com md5 online
+    secret: process.env.APP_SECRET, // => gerado com md5 online
     expiresIn: '1d',
   },
 };
