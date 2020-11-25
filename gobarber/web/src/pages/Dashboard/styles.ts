0c072bae74ba4fbb2dcc2145ccd92d175465d452
Murilo Cunha <murilo.sant@hotmail.com>
2020-11-25 10:09:09 -0300

    feat(dashboard): added calendar and styles

--- gobarber/web/src/pages/Dashboard/styles.ts
+++ gobarber/web/src/pages/Dashboard/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Container = styled.div``;
 
@@ -243,4 +244,57 @@ export const Appointment = styled.div`
 
 export const Calendar = styled.aside`
   width: 380px;
+
+  .DayPicker {
+    border-radius: 10px;
+    background: #28262e;
+  }
+
+  .DayPicker-wrapper {
+    padding-bottom: 0;
+  }
+
+  .DayPicker,
+  .DayPicker-Month {
+    width: 100%;
+  }
+
+  .DayPicker-Month {
+    margin: 16px;
+    border-spacing: 8px;
+    border-collapse: separate;
+  }
+
+  .DayPicker-Day {
+    width: 40px;
+    height: 40px;
+  }
+
+  .DayPicker-Day--available:not(.DayPicker-Day--outside) {
+    border-radius: 10px;
+
+    color: #fff;
+    background: #3e3b47;
+  }
+
+  .DayPicker:not(.DayPicker--interactionDisabled)
+    .DayPicker-Day:not(.DayPicker-Day--disabled):not(.DayPicker-Day--selected):not(.DayPicker-Day--outside):hover {
+    background: ${shade(0.2, '#3e3b47')};
+  }
+
+  .DayPicker-Day--today {
+    font-weight: normal;
+  }
+
+  .DayPicker-Day--disabled {
+    color: #666360 !important;
+    background: transparent !important;
+  }
+
+  .DayPicker-Day--selected {
+    border-radius: 10px;
+
+    color: #232129 !important;
+    background: #ff9000 !important;
+  }
 `;
