c2685579068536a5b4a7c4835139d37e07fd8d8c
Murilo Cunha <murilo.sant@hotmail.com>
2020-11-24 21:56:22 -0300

    feat(dashboard): starting content layout: next appointments

--- gobarber/web/src/pages/Dashboard/styles.ts
+++ gobarber/web/src/pages/Dashboard/styles.ts
@@ -64,3 +64,113 @@ export const Profile = styled.div`
     }
   }
 `;
+
+export const Content = styled.main`
+  display: flex;
+
+  max-width: 1120px;
+  margin: 64px auto;
+`;
+
+export const Schedule = styled.section`
+  flex: 1;
+  margin-right: 120px;
+
+  h1 {
+    font-size: 36px;
+  }
+
+  p {
+    display: flex;
+    align-items: center;
+
+    margin-top: 8px;
+    font-weight: 500;
+
+    color: #ff9000;
+
+    span {
+      display: flex;
+      align-items: center;
+    }
+
+    span + span::before {
+      width: 1px;
+      height: 12px;
+
+      margin: 0 8px;
+
+      content: '';
+      background: #ff9000;
+    }
+  }
+`;
+
+export const NextAppointment = styled.div`
+  margin-top: 64px;
+
+  strong {
+    font-size: 20px;
+    font-weight: 400;
+
+    color: #999591;
+  }
+
+  div {
+    position: relative;
+
+    display: flex;
+    align-items: center;
+
+    margin-top: 10px;
+
+    padding: 16px 24px;
+    border-radius: 10px;
+
+    background: #3e3b47;
+
+    &::before {
+      position: absolute;
+      top: 10%;
+      left: 0;
+
+      width: 1px;
+      height: 80%;
+
+      content: '';
+      background: #ff9000;
+    }
+
+    img {
+      width: 80px;
+      height: 80px;
+      border-radius: 50%;
+
+      object-fit: cover;
+    }
+
+    strong {
+      margin-left: 24px;
+      color: #fff;
+    }
+
+    span {
+      display: flex;
+      align-items: center;
+
+      margin-left: auto;
+
+      color: #999591;
+
+      svg {
+        margin-right: 8px;
+
+        color: #ff9000;
+      }
+    }
+  }
+`;
+
+export const Calendar = styled.aside`
+  width: 380px;
+`;
